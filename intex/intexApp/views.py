b9f3432e8c7b6e2bf658ca26ec89647978586fad
Saberrytwo <90337853+Saberrytwo@users.noreply.github.com>
2021-12-02 13:01:41 -0700

    urls, views, connected database, boostrap start

--- intex/intexApp/views.py
+++ intex/intexApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def prescriberPageView(request) :
+    return render(request, 'intexApp/prescriber.html')
+
+def drugPageView(request) :
+    return render(request, 'intexApp/drug.html')
